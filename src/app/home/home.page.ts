ac65db4fe1fca62e4913397591b642411e0d162c
littletheprogrammer@gmail.com <littletheprogrammer@gmail.com>
2019-07-20 12:04:32 +0000

    ionic view is disturbing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {Camera, CameraOptions} from "@ionic-native/camera/ngx"
+import { AlertController } from '@ionic/angular';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  currentImage:any;
+  constructor(private camera:Camera, private alert:AlertController, private http:HttpClient) {}
 
-  constructor() {}
 
+    //alert
+    async showAlert(message){
+      const alet = await this.alert.create({
+        header: 'test',
+        subHeader: 'another test',
+        message: message,
+        buttons: ['ok']
+      });
+
+      await alet.present()
+    }
+
+   takePicture(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     
+     this.currentImage = 'data:image/jpeg;base64,' + imageData;
+     if(this.currentImage){
+
+      this.http.post('finllect.herokuapp.com/test_upload', {key:'img', Payload:this.currentImage}).subscribe(res => {
+        this.showAlert(res)
+      })
+      //this.showAlert(this.currentImage)
+     }
+    }, (err) => {
+     // Handle error
+    });
+   }
 }
